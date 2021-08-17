b4024760a503a13dad6f85bcf5743ee5df481be2
Suriya Senthilkumar <54205777+suriya-it19@users.noreply.github.com>
2021-08-17 13:35:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+!pip install opencv-python
+!apt update && apt install -y libsm6 libxext6
+!apt-get install -y libxrender-dev
+
 import streamlit as st
 from multiapp import MultiApp
 from apps import homepage
